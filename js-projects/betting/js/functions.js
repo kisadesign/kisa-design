59d5c2a312f48508c3864c3df98a8bb94e7d37c5
Mark Bensley <mark@kisadesign.co.uk>
2015-11-23 15:06:50 +0000

    All functionality working and regex implemented

--- js-projects/betting/js/functions.js
+++ js-projects/betting/js/functions.js
@@ -11,7 +11,14 @@ var horses = [
     userName = prompt('Please enter your name:'),
     playMoney = 100.00,
     betValue = 0;
-document.getElementById('player-name').innerHTML = userName;
+
+    if(/^([a-zA-Z]+\s)*[a-zA-Z]+$/.test(userName)){
+        document.getElementById('player-name').innerHTML = userName;
+    }
+    else {
+        //Do something for no match
+    }
+
 document.getElementById('player-pot').innerHTML = playMoney;
 
 var listHorses = document.getElementById('horses');
@@ -28,102 +35,165 @@ function placeBet(){
     //betValue is the amount bet
     betValue = document.getElementById('bet-placed').value;
 
-    //Check players balance
-    if (playMoney <= 0){
-        alert("You do not have enough money to place any more bets");
+    if(!betValue || !/^[0-9]+$/.test(betValue)){
+        alert('Enter amount');
     }
-    else if (betValue > playMoney) {
-        alert("Your maximum bet that can be placed is:" + playMoney);
-        document.getElementById('bet-placed').value = playMoney;
-    }
-
     else{
-        playMoney -= betValue;
-        document.getElementById('player-pot').innerHTML = playMoney;
 
-        //Gets the sleceted horse chossen by user
-        for (var run = 0; run < runners.length; run++){
-            if(runners[run].checked){
-                var runnerId = runners[run].value;
-                betValue = Number(betValue);
-                break;
-            }
+        //Check players balance
+        if (playMoney <= 0){
+            alert("You do not have enough money to place any more bets");
+        }
+        else if (betValue > playMoney) {
+            alert("Your maximum bet that can be placed is:" + playMoney);
+            document.getElementById('bet-placed').value = playMoney;
         }
 
-        //Checks the users chosen horse against the list of hosrses to collect all data about chosen horse
-        for (var findHorses = 0; findHorses < horses.length; findHorses++) {
-            if(runnerId == horses[findHorses].number){
-                var chossenHorseNumber = horses[findHorses].number,
-                    chossenHorseName = horses[findHorses].name,
-                    chossenHorseOdds = horses[findHorses].odds;
+        else{
+            playMoney -= betValue;
+            document.getElementById('player-pot').innerHTML = playMoney;
+
+            //Gets the sleceted horse chossen by user
+            for (var run = 0; run < runners.length; run++){
+                if(runners[run].checked){
+                    var runnerId = runners[run].value;
+                    betValue = Number(betValue);
+                    break;
+                }
             }
-        }
 
-        //Add all ods together, horses odds divided by whole odss number times 100
-        //Find total odds
-        var totalOdds = 0;
-        for(var horseOdds in horses ){
-            totalOdds += horses[horseOdds].odds;
-        }
+            if(runnerId){
 
-        var horseOddsPerc = 0,
-            finalScore = 0,
-            randomRace = 0,
-            raceSpeed = 0;
-        for(var horsePerc = 0; horsePerc < horses.length; horsePerc++){
-            horseOddsPerc = horses[horsePerc].odds;
-            horseOddsPerc = horseOddsPerc / totalOdds * 100;
-            horseOddsPerc = Math.round(horseOddsPerc);
+                //Checks the users chosen horse against the list of hosrses to collect all data about chosen horse
+                for (var findHorses = 0; findHorses < horses.length; findHorses++) {
+                    if(runnerId == horses[findHorses].number){
+                        var chossenHorseNumber = horses[findHorses].number,
+                            chossenHorseName = horses[findHorses].name,
+                            chossenHorseOdds = horses[findHorses].odds;
+                    }
+                }
 
-            if(horseOddsPerc <= 15){
-                raceSpeed = 1;
-                randomRace = Math.floor((Math.random() * 5) + 1);
-                horses[horsePerc].raceSpeed = raceSpeed;
-                horses[horsePerc].randomRace = randomRace;
-                horses[horsePerc].racePercentage = horseOddsPerc;
-                finalScore = raceSpeed + randomRace;
-                horses[horsePerc].finalScore = finalScore;
+                //Add all ods together, horses odds divided by whole odss number times 100
+                //Find total odds
+                var totalOdds = 0;
+                for(var horseOdds in horses ){
+                    totalOdds += horses[horseOdds].odds;
+                }
+
+                var horseOddsPerc = 0,
+                    finalScore = 0,
+                    randomRace = 0,
+                    raceSpeed = 0;
+                for(var horsePerc = 0; horsePerc < horses.length; horsePerc++){
+                    horseOddsPerc = horses[horsePerc].odds;
+                    horseOddsPerc = horseOddsPerc / totalOdds * 100;
+                    horseOddsPerc = Math.round(horseOddsPerc);
+
+                    if(horseOddsPerc <= 15){
+                        raceSpeed = 1;
+                        randomRace = Math.floor((Math.random() * 10) + 1);
+                        horses[horsePerc].raceSpeed = raceSpeed;
+                        horses[horsePerc].randomRace = randomRace;
+                        horses[horsePerc].racePercentage = horseOddsPerc;
+                        finalScore = raceSpeed + randomRace;
+                        horses[horsePerc].finalScore = finalScore;
 
 
-            }
-            else if (horseOddsPerc >= 16 && horseOddsPerc < 20){
-                raceSpeed = 2;
-                randomRace = Math.floor((Math.random() * 5) + 1);
-                horses[horsePerc].raceSpeed = raceSpeed;
-                horses[horsePerc].randomRace = randomRace;
-                horses[horsePerc].racePercentage = horseOddsPerc;
-                finalScore = raceSpeed + randomRace;
-                horses[horsePerc].finalScore = finalScore;
-            }
-            else if (horseOddsPerc >= 21 && horseOddsPerc < 35){
-                raceSpeed = 3;
-                randomRace = Math.floor((Math.random() * 5) + 1);
-                horses[horsePerc].raceSpeed = raceSpeed;
-                horses[horsePerc].randomRace = randomRace;
-                horses[horsePerc].racePercentage = horseOddsPerc;
-                finalScore = raceSpeed + randomRace;
-                horses[horsePerc].finalScore = finalScore;
+                    }
+                    else if (horseOddsPerc >= 16 && horseOddsPerc < 20){
+                        raceSpeed = 2;
+                        randomRace = Math.floor((Math.random() * 10) + 1);
+                        horses[horsePerc].raceSpeed = raceSpeed;
+                        horses[horsePerc].randomRace = randomRace;
+                        horses[horsePerc].racePercentage = horseOddsPerc;
+                        finalScore = raceSpeed + randomRace;
+                        horses[horsePerc].finalScore = finalScore;
+                    }
+                    else if (horseOddsPerc >= 21 && horseOddsPerc < 35){
+                        raceSpeed = 3;
+                        randomRace = Math.floor((Math.random() * 10) + 1);
+                        horses[horsePerc].raceSpeed = raceSpeed;
+                        horses[horsePerc].randomRace = randomRace;
+                        horses[horsePerc].racePercentage = horseOddsPerc;
+                        finalScore = raceSpeed + randomRace;
+                        horses[horsePerc].finalScore = finalScore;
+                    }
+                    else {
+                        raceSpeed = 4;
+                        randomRace = Math.floor((Math.random() * 10) + 1);
+                        horses[horsePerc].raceSpeed = raceSpeed;
+                        horses[horsePerc].randomRace = randomRace;
+                        horses[horsePerc].racePercentage = horseOddsPerc;
+                        finalScore = raceSpeed + randomRace;
+                        horses[horsePerc].finalScore = finalScore;
+                    }
+
+                    console.log(horses[horsePerc].raceSpeed);
+                }
+
+                //Check to see if more than one horse has the same final score
+                var extraRace = [];
+
+
+                //Finds the lowest number for finalScore
+                var minFinalScore = horses.reduce(function(minFinalScore, obj) {
+                    return obj.finalScore < minFinalScore ? obj.finalScore : minFinalScore;
+                }, Infinity);
+
+                //Looks at all the horses and push all horses numbers with the lowest score to a new array
+                for (var findWinner in horses )
+                {
+                    if(minFinalScore == horses[findWinner].finalScore){
+                        extraRace.push(horses[findWinner].number);
+                    }
+                }
+
+
+
+                console.log('Lowest score ' + minFinalScore);
+
+                //Push out come of extraRace to a new array and check its length
+                var betReturn = betValue * chossenHorseOdds + betValue,
+                    headWinner;
+                if (extraRace.length >= 2){
+                    //Select a random winner if theres more than one horse with the same low score
+                    headWinner = extraRace[Math.floor(Math.random() * extraRace.length)]
+                    console.log('There are more than one winner: ' + extraRace.length);
+                    console.log('Won by a nose' + headWinner);
+                    console.log('You chose horse number: ' + chossenHorseNumber);
+                }
+                else {
+                    //The winner is selected
+                    headWinner = extraRace;
+                    console.log('There was only one winner horse number ' + extraRace);
+                    console.log('You chose horse number: ' + chossenHorseNumber);
+                }
+
+                if (headWinner == chossenHorseNumber){
+                    alert('You Won!! £' + betReturn + ' has been added to your account.');
+                    playMoney += betReturn;
+                    document.getElementById('player-pot').innerHTML = playMoney;
+
+                }
+                else
+                {
+                    alert('You lost please try again');
+                }
             }
             else {
-                raceSpeed = 4;
-                randomRace = Math.floor((Math.random() * 5) + 1);
-                horses[horsePerc].raceSpeed = raceSpeed;
-                horses[horsePerc].randomRace = randomRace;
-                horses[horsePerc].racePercentage = horseOddsPerc;
-                finalScore = raceSpeed + randomRace;
-                horses[horsePerc].finalScore = finalScore;
+                alert('no horse selected');
             }
 
-            console.log(horses[horsePerc].raceSpeed);
+
+
+
+
+
         }
 
-        var betReturn = betValue * chossenHorseOdds + betValue;
-
-
-        console.log(typeof betReturn, typeof betValue);
-        console.log(horses);
-        alert('Congratulations your return is: ' + betReturn);
     }
+
+
 
 
 
