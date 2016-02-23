57a29e0a87f6c0abe01d54984e4eb7a32a7d8e45
Mark Bensley <mark@kisadesign.co.uk>
2016-02-23 17:14:20 +0000

    styling added to slider

--- client-testing/worth-retail/js/functions.js
+++ client-testing/worth-retail/js/functions.js
@@ -4,12 +4,31 @@
 
 $(document).ready(function(){
     $('.wr-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
-        arrows: true,
-        arrowPrev: "<p>Hello world</p>"
+        autoplaySpeed: 4000,
+        speed: 1000,
+        nextArrow: '<div class="wr-slide-arrow-next"><i class="fa fa-angle-right"></i></div>',
+        prevArrow: '<div class="wr-slide-arrow-prev"><i class="fa fa-angle-left"></i></div>',
+        pauseOnHover: false
     });
+
+
+
+    var windowSize = $(window).height(),
+        windowSubSize = windowSize - 147;
+    console.log(windowSize + " " + windowSubSize);
+
+    var c = windowSubSize / windowSize,
+        d = c * 100;
+
+    $('.wr-max-height').css("height", windowSubSize);
+    $(window).resize(function(){
+        var windowSize = $(window).height(),
+            windowSubSize = windowSize - 147;
+        $('.wr-max-height').css("height", windowSubSize);
+    })
+
+
+
 });
 
