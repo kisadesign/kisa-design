e80f5fbeae9dc21d04695115ac56edbe8c20f0fd
Mark Bensley <mark@kisadesign.co.uk>
2016-02-23 14:46:36 +0000

    Slider added

--- client-testing/worth-retail/js/functions.js
+++ client-testing/worth-retail/js/functions.js
@@ -1,3 +1,15 @@
 /**
  * Created by markbensley on 23/02/16.
  */
+
+$(document).ready(function(){
+    $('.wr-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        arrowPrev: "<p>Hello world</p>"
+    });
+});
+
