9ae650ed9d36d580480eefedb77c952a7e2ffb88
Mark Bensley <mark@kisadesign.co.uk>
2016-02-23 10:18:41 +0000

    Slider setup

--- client-testing/pw/js/functions.js
+++ client-testing/pw/js/functions.js
@@ -4,5 +4,11 @@
 
 
 $(document).ready(function(){
-    $('.pw-hero-banner').slick();
+    $('.pw-hero-banner--container').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1000,
+        nextArrow: '<p>Hello</p>',
+        prevArrow: '<button type="button" class="ct-slick-prev">Previous</button>'
+    });
 });
