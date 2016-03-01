d22bb787d50813c68ccf7873f859031a34c07ad3
Mark Bensley <mark@kisadesign.co.uk>
2016-03-01 12:14:16 +0000

    Map api added

--- client-testing/worth-retail/map.js
+++ client-testing/worth-retail/map.js
@@ -2,16 +2,30 @@
  * Created by markbensley on 29/02/16.
  */
 function initMap() {
+    var myLocation = {lat : 51.5539439, lng : -0.1447536},
+        markerImage = '../img/maps-icon.png';
+
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 0, lng: 0},
-        zoom: 3,
-        styles: [{
-            featureType: 'poi',
-            stylers: [{ visibility: 'off' }]  // Turn off points of interest.
-        }, {
-            featureType: 'transit.station',
-            stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
-        }],
+        zoom: 17,
+        scrollwheel: false,
+        mapTypeControl: false,
+        center: myLocation,
+        //styles: [{
+        //    featureType: 'poi',
+        //    stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+        //}, {
+        //    featureType: 'transit.station',
+        //    stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+        //}],
         disableDoubleClickZoom: true
     });
+
+    var myMarker = new google.maps.Marker({
+        position : myLocation,
+        map : map
+        //icon : markerImage
+    });
+
+
+
 }
